ca27dcd3369ad3c246613e06e3c09ba4093b19a0
V <vlad.vlad.1234@yandex.ru>
2022-08-09 16:20:08 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        String firstName = "Ivan ";
+        String middleName  = "Ivanovich ";
+        String lastName  = "Ivanov";
+        String fullName = "ФИО сотрудника — " + firstName + middleName + lastName;
+        System.out.println(fullName);
     }
 }
